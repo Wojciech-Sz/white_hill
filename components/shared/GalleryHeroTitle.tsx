c54352e33323faacbbf4614ffd39700ccb75dd97
WojciechSz <w.szczygielski00@gmail.com>
2024-09-08 16:32:08 +0200

    chevron position

--- components/shared/GalleryHeroTitle.tsx
+++ components/shared/GalleryHeroTitle.tsx
@@ -17,7 +17,7 @@ const GalleryHeroTitle = ({
   };
   return (
     <div className="wrapper flex-center relative size-full flex-col text-white">
-      <div className="grid w-full grid-rows-[1fr_auto_1fr] justify-center lg:justify-normal">
+      <div className="grid w-max grid-rows-[1fr_auto_1fr] justify-center lg:w-full lg:justify-normal">
         <div />
         <div className="flex-center flex-col">
           <h2 className="section-title">
@@ -26,11 +26,11 @@ const GalleryHeroTitle = ({
           <ChevronDown
             id="chevron"
             onMouseDown={() => handleClick()}
-            className="relative -top-2 z-[1] size-8 cursor-pointer transition-transform duration-300 hover:translate-y-2 hover:scale-125 sm:size-10 lg:size-14"
+            className="relative -top-2 z-[1] size-8 translate-x-[.5px] cursor-pointer transition-transform duration-300 hover:translate-y-2 hover:scale-125 xs:translate-x-[2.5px] sm:size-10 sm:translate-x-0 lg:size-14"
           />
         </div>
         <div
-          className={`projects-links-container relative -top-3 translate-x-[-2.5px] sm:translate-x-[-3px] md:-translate-x-1 lg:-translate-x-2.5 ${isDown ? "mb-2 h-9 opacity-100" : "mb-0 h-0 overflow-hidden opacity-0"}`}
+          className={`projects-links-container relative -top-3 translate-x-[-2.5px] sm:translate-x-[-2.5px] md:translate-x-[-3.5px] lg:-translate-x-2.5 ${isDown ? "mb-2 h-9 opacity-100" : "mb-0 h-0 overflow-hidden opacity-0"}`}
         >
           {projectLinks.map((link, i) => (
             <div
